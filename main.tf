58079fbdcd104d3fa45551ceed10bb0b10bb250d
Michael Oludoyi <47690684+eureka4eva@users.noreply.github.com>
2023-01-26 22:42:55 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,1 @@
-resource "azurerm_resource_group" "azureinfra_rg" {
-  name     = "azure-infra-rg"
-  location = "West Europe"
-}
 
-resource "azurerm_app_service_plan" "cicd_app" {
-  name                = "cicd-app-service"
-  location            = "West Europe"
-  kind                = "Linux"
-  reserved            = true
-  resource_group_name = azurerm_resource_group.azureinfra_rg.name
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
-}
